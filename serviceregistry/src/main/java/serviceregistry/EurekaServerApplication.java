a47d40a88b9153302519d2cee4dfc11dd49798e3
Will Ferguson <william.ferguson@travelex.com>
2019-01-29 17:22:21 +0000

    Configuration for running in ECS

--- serviceregistry/src/main/java/serviceregistry/EurekaServerApplication.java
+++ serviceregistry/src/main/java/serviceregistry/EurekaServerApplication.java
@@ -1,14 +1,28 @@
 package serviceregistry;
 
+import com.netflix.appinfo.AmazonInfo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.commons.util.InetUtils;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+    @Bean
+    @Profile("!default")
+    public EurekaInstanceConfigBean eurekaInstanceConfig(InetUtils inetUtils) {
+        EurekaInstanceConfigBean b = new EurekaInstanceConfigBean(inetUtils);
+        AmazonInfo info = AmazonInfo.Builder.newBuilder().autoBuild("eureka");
+        b.setDataCenterInfo(info);
+        return b;
     }
 }
 
